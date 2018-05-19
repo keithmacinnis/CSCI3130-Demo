e9807a758b0e65563c9bb7aad2a1609f9d3aa6d8
Keith MacInnis <keithmacinnis@gmail.com>
2018-05-18 21:02:35 -0300

    added a button, a textfield, and a sendMessage function to MainActivity to copy text from textfield to label

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sendMessage(View view) {
+        //Grab input
+        EditText editText = findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        //Throw input onto the label
+        TextView destinationText = findViewById(R.id.destinationText);
+        destinationText.setText(message);
+    }
 }
+
+
+//
